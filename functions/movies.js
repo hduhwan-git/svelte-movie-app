de9e2c774c33669af995b6dad59dbb2296b6aafa
Doo Hwan <hduhwan@naver.com>
2022-10-11 01:47:22 +0900

    netlify handler error

--- functions/movies.js
+++ functions/movies.js
@@ -1,3 +1,14 @@
-exprots.handler  = async (event, context) => {
-  
-}; 
+import { Handler } from "@netlify/functions";
+
+const handler  = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+       message: "netlify/functions test", 
+       name:'Robin', 
+       email:'hduhwan@gmail.com'
+      }),
+  };
+};
+
+export { handler };
